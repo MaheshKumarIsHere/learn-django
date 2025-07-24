02272da2e4b101e2dbc0ebeec2986a64aeeec48c
mahesh <maheshkumar@gmail.com>
2025-07-24 20:13:23 +0530

    map route added with leaflet

--- src/ch1/views.py
+++ src/ch1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Home page")
+    return render(request,"ch1/home.html")
+
+
+def map(request):
+    return render(request,"ch1/map.html")
